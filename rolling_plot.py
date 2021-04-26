40d04b56c9cfc392bdd099f175d4ceb9cdce0d42
Matthew Obradovic <mo16@illinois.edu>
2021-04-26 15:03:12 -0500

    Updated rolling_plot.py to work with an arbitrary number of lines

--- rolling_plot.py
+++ rolling_plot.py
@@ -5,8 +5,6 @@ import time
 import serial
 import getfloat
 
-start_time = time.time()
-
 port = 'COM5'
 baud = 921600
 timeout = 1
@@ -15,72 +13,60 @@ yupper = 100.5
 bufwidth = 300
 numlines = 7
 
+# set start time
+start_time = time.time()
+
+# set up serial port
 ser = serial.Serial(port, baud, timeout = 1)
 
+#set up figue
 fig, ax = plt.subplots()
 ax.set_ylim(ylower,yupper)
 line, = ax.plot([], [], lw = 2)
 
 plotlays, plotcols = [numlines], ["black","red","blue","green","yellow","purple","orange"]
-lines = []
+lines = [] # lines holds line objects for ploting
+data = [] # data holds data points to plot
+
+# only need one list t (time) for all lines
+t = [0]*bufwidth
+
 for index in range(numlines):
-    lobj = ax.plot([], [], lw = 2, color=plotcols[index])[0]
-    lines.append(lobj)
+    # create line object
+    lineobj = ax.plot([], [], lw = 2, color=plotcols[index])[0]
+    lines.append(lineobj)
+    # create data object
+    dataobj = [0]*bufwidth
+    data.append(dataobj)
 
 def init():  # only required for blitting to give a clean slate.
     for line in lines:
         line.set_data([],[])
     return lines
 
-x1, x2, x3, x4, x5, x6, x7 = [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth
-y1, y2, y3, y4, y5, y6, y7 = [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth, [0]*bufwidth
 
-def animate(data):
-    #line.set_ydata(np.sin(i/50) )  # update the data.
-    t = time.time()-start_time
+def animate(i):
+    # delete first entry in each list
+    del t[0]
+    for index in range(numlines):
+        del (data[index])[0]
 
-    # not sure why this is here
-    del x1[0]
-    del x2[0]
-    del x3[0]
-    del x4[0]
-    del x5[0]
-    del x6[0]
-    del x7[0]
-    del y1[0]
-    del y2[0]
-    del y3[0]
-    del y4[0]
-    del y5[0]
-    del y6[0]
-    del y7[0]
-
+    # get data
     f_arr = getfloat.get_floats(ser,7)
-    print (f_arr)
+    #print (f_arr)
 
-    x1.append(t)
-    x2.append(t)
-    x3.append(t)
-    x4.append(t)
-    x5.append(t)
-    x6.append(t)
-    x7.append(t)
-    y1.append(float(*f_arr[0]))
-    y2.append(float(*f_arr[1]))
-    y3.append(float(*f_arr[2]))
-    y4.append(float(*f_arr[3]))
-    y5.append(float(*f_arr[4]))
-    y6.append(float(*f_arr[5]))
-    y7.append(float(*f_arr[6]))
-    
-    xlist = [x1, x2, x3, x4, x5, x6, x7]
-    ylist = [y1, y2, y3, y4, y5, y6, y7]
+    # append new data to lists
+    t.append(time.time()-start_time)
+    for index in range(numlines):
+        data[index].append(float(*f_arr[index]))    
 
+    #rescale plot
     ax.relim()
     ax.autoscale_view()
     
+    # set line data
     for lnum, line in enumerate(lines):
-        line.set_data(xlist[lnum], ylist[lnum])
+        line.set_data(t, data[lnum])
 
     return lines
 
